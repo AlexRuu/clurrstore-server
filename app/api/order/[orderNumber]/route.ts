25fa7cf65f09c3b1f4c654fd8d8281522686d59d
Alex <weiru@hotmail.ca>
2024-02-29 16:28:03 -0500

    Create order api

---
+++ app/api/order/[orderNumber]/route.ts
@@ -0,0 +1,27 @@
+import prismadb from "@/lib/prismadb";
+import { NextResponse } from "next/server";
+
+export async function GET(
+  _req: Request,
+  { params }: { params: { orderNumber: string } }
+) {
+  try {
+    if (!params.orderNumber) {
+      return new NextResponse("Please provide an order ID", { status: 401 });
+    }
+
+    const order = await prismadb.order.findFirst({
+      where: {
+        orderNumber: params.orderNumber,
+      },
+      include: {
+        orderItem: { include: { product: true, design: true, style: true } },
+      },
+    });
+    console.log(order);
+    return NextResponse.json(order);
+  } catch (error) {
+    console.log("[ORDER_GET] error", error);
+    return new NextResponse("Error retrieving order", { status: 500 });
+  }
+}
